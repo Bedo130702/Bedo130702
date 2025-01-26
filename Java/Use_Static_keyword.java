8797852ec7b0fbd34586eb52bc5abd7a5563bc8d
Beda130702 <patrabedabrata352@gmail.com>
2025-01-26 17:09:37 +0530

    Update Use_Static_keyword.java

--- Java/Use_Static_keyword.java
+++ Java/Use_Static_keyword.java
@@ -2,17 +2,20 @@
     String name;
     int roll;
     static String school;
+    public static void changeSchoolname(){
+        school="Satsang Tapovan School";
+    }
     public void printInfo(){
         System.out.println(name+"\n"+ roll+"\n"+school);
     }
     // ***** Copy Constructor *****
-    Student(Student s2){
-        this.name=s2.name;
-        this.roll=s2.roll;
-    }
-    Student(){
-
-    }
+//    Student(Student s2){
+//        this.name=s2.name;
+//        this.roll=s2.roll;
+//    }
+//    Student(){
+//
+//    }
  }
 public class USE_Static_KeyWord{
     public static  void  main(String args[]){
@@ -20,8 +23,11 @@ public class USE_Static_KeyWord{
         Student s1=new Student();
         s1.name="Bedabrata Patra";
         s1.roll=1;
-//        s1.printInfo();
-        Student s2=new Student(s1);
-        s2.printInfo();
+        s1.printInfo();     // Static school property print
+        s1.changeSchoolname();  // Static school method
+        s1.printInfo();     // After static method call it change school name.
+     // Copy Constructor..
+//        Student s2=new Student(s1);
+//        s2.printInfo();
     }
 }
